3218c0f74804323ca5eae579500c87a4b660918d
JamesWanjohi <jnyaga@rcmrd.org>
2021-07-20 16:27:24 +0300

    worked on select of counties using a shapefile in GEE

--- tethysapp/rdst/ajax_controller.py
+++ tethysapp/rdst/ajax_controller.py
@@ -1,6 +1,6 @@
 import json
 from django.http import JsonResponse
-from .geefunctions import getMap 
+from .geefunctions import getMap, clippedMap 
 
 def get_map(request):
 	return_obj = {}
@@ -16,6 +16,28 @@ def get_map(request):
 			collectionName = info.get('collection')
 			map_object = getMap(collectionName, visParams, reducer, time_start, time_end)
 			print(map_object)
+			return_obj["url"] = map_object
+			return_obj["success"] = "success"
+
+		except Exception as e:
+			return_obj["error"] = "Error processing Request. Error: "+ str(e)
+	print(return_obj)
+	return JsonResponse(return_obj)
+
+def get_cmap(request):
+	return_obj = {}
+
+	if request.method == "POST":
+		try:
+			info = request.POST
+			option2 = info.get('option2')
+			time_start = info.get('time_start', None)
+			time_end = info.get('time_end', None)
+			visParams = json.loads(info.get('visparams', None))
+			reducer = info.get('reducer', None)
+			collectionName = info.get('collection')
+			county = info.get('county')
+			map_object = clippedMap(collectionName, visParams, reducer, time_start, time_end, county)
 			return_obj["url"] = map_object
 			return_obj["success"] = "success"
 
